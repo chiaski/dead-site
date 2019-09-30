468ad6e7415d97a9615dc8d8149ae559414e9473
Chia <chiamisola@gmail.com>
2019-09-30 16:57:59 -0400

    Add mve animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,3 +17,47 @@ function moveCursor(e) {
 }
 
 $(window).on('mousemove', moveCursor);
+
+$("#sayhello").mouseenter(function(){
+    $("#landing").css("background-image", "url(assets/images/sayhello.jpg)");
+    console.log("hello");
+});
+
+$("#sayhello").mouseleave(function(){
+    
+    $("#landing").css("background-image", "url()");
+});
+
+
+
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveBackground() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+  
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('.bg').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveBackground);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+
+});
+
+moveBackground();
