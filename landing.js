3ec61cbd616933c133c7dff169dd824e47fb7ecc
Chia Amisola <chiamisola@gmail.com>
2021-01-16 17:03:52 -0500

    better masonry loader

---
+++ landing.js
@@ -0,0 +1,19 @@
+
+//    var colors = ["#db635b", "#e3abbc", "#bfbfbf", "#81a391", "#9a97d1", "#8db1ad", "#f0cb7d", "#d67a90", "#ACB2B4"];
+//
+//    const choose = colors[Math.floor(Math.random() * colors.length)];
+//
+//    document.body.style.background = choose;
+//
+//    var links = document.getElementById("navigation").getElementsByTagName("a");
+//
+//    for (var i = 0; i < links.length; i++) {
+//      //      links[i].style.color = choose;
+//    }
+//
+//    function color() {
+//      const choose = colors[Math.floor(Math.random() * colors.length)];
+//
+//      document.body.style.background = choose;
+//    }
+    
